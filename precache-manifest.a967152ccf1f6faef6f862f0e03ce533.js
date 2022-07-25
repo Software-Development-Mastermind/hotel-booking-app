831465fdd21494ba26a312447b3d7def78ecab0a
Andy Sterkowitz <admin@andysterkowitz.com>
2022-07-25 18:47:24 -0500

    Updates

---
+++ precache-manifest.a967152ccf1f6faef6f862f0e03ce533.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54eee8fdd86af340e1c15c7a8d6a2ccc",
+    "url": "/hotel-booking-app/index.html"
+  },
+  {
+    "revision": "f69726438e28a48f5f5e",
+    "url": "/hotel-booking-app/static/css/main.99fd8aa7.chunk.css"
+  },
+  {
+    "revision": "6be82447454746d16837",
+    "url": "/hotel-booking-app/static/js/2.06af3692.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hotel-booking-app/static/js/2.06af3692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f69726438e28a48f5f5e",
+    "url": "/hotel-booking-app/static/js/main.f6a79b73.chunk.js"
+  },
+  {
+    "revision": "46a4924ff621289a84c5",
+    "url": "/hotel-booking-app/static/js/runtime-main.801e3816.js"
+  },
+  {
+    "revision": "8045a22a02e7a0d3e952044ab4633e0e",
+    "url": "/hotel-booking-app/static/media/pittsburgh.8045a22a.jpg"
+  }
+]);
